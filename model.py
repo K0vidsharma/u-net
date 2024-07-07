aac1fa029fa308e2f9a98ba0f296574700d6cfe7
Kovid <kovid2003@gmail.com>
2024-07-07 17:04:55 +0530

    Added model.py code

--- model.py
+++ model.py
@@ -44,8 +44,44 @@ class UNET(nn.Module):
         self.bottom = DoubleConv(features[-1], features[-1]*2)
         self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
 
-    def forward(self,x):
-        pass
+    def forward(self, x):
+        skip_connections = []
+
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+
+        x = self.bottom(x)
+
+        skip_connections = skip_connections[::-1]
+
+        for idx in range(0, len(self.ups), 2):
+            x = self.ups[idx](x)
+            skip_connection = skip_connections[idx // 2]
+
+            if x.shape != skip_connection.shape:  # Required if we have not chosen the input and output size
+                # according to MaxPool2d
+                x = ft.resize(x, size=(skip_connection.shape[2:]))
+            concat_skip = torch.concat((skip_connection, x), dim=1)
+
+            x = self.ups[idx+1](concat_skip)
+
+        return self.final_conv(x)
+
+
+def test_model():
+    x = torch.randn((3, 1, 572, 572))
+    model = UNET(in_channels=1, out_channels=1)
+
+    preds = model(x)
+    print(preds.shape)
+    print(x.shape)
+    assert preds.shape == x.shape
+
+
+if __name__ == "__main__":
+    test_model()
 
 
 
