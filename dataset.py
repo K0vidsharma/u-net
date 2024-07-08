4041292bf8e713dd67667f098a3d07620a5f977a
Kovid <kovid2003@gmail.com>
2024-07-08 22:06:51 +0530

    Added Dataset Code

--- dataset.py
+++ dataset.py
@@ -19,5 +19,13 @@ class LungImageDataset(Dataset):
     def __getitem__(self, index):
         img_path = os.path.join(self.image_dir, self.images[index])
         mask_path = os.path.join(self.mask_dir, self.masks[index])
-        image = np.array(Image.open(img_path))
-        mask = np.array()
+        image = np.array(Image.open(img_path).convert("RGB"))
+        mask = np.array(Image.open(mask_path).convert("L"), dtype=np.float32)
+        mask[mask == 255] = 1.0
+
+        if self.transform:
+            augmentations = self.transform(image=image, mask=mask)
+            image = augmentations["image"]
+            mask = augmentations["mask"]
+
+        return image, mask
